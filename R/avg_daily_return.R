87b20b08abad5a6fed4779c2dee8c48ad73cc062
YINAN GUO <alex.yinan.guo@gmail.com>
2022-01-18 04:28:21 -0800

    add R file

---
+++ R/avg_daily_return.R
@@ -0,0 +1,9 @@
+#' Outputs the average daily return of the inputted cryptocurrency price.
+#'
+#' @param name A numeric vector representing the inputted cryptocurrency price.
+#'
+#' @return A numeric value representing the average daily return of the cryptocurrency.
+#'
+#' @examples
+#' BTCBitcoin <- c(52254.2, 53981.5, 52112.9)
+#' avg_daily_return("BTCBitcoin")
