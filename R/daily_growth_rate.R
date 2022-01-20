b7f5baef0444a2a417acb844ced3703207e211f2
nobbynguyen <thungoct283@gmail.com>
2022-01-19 22:02:56 -0800

    docs: Add specification of func daily_grow_rate

---
+++ R/daily_growth_rate.R
@@ -0,0 +1,11 @@
+#' Calculate daily growth rate of a cryptocurrency
+#'
+#' @param df A Data frame with date and price data.
+#' @param col_name a string indicate the column holding daily closing price data.
+#'
+#' @return A dataframe with a new column of daily growth rater.
+#' @export
+#'
+#' @examples
+#' daily_growth_rate(price_df, closing_price)
+
