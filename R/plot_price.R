4ecdacce553b7a354553a03a08a53c174f378893
Cici Du <cicidhz@gmail.com>
2022-01-19 19:41:51 -0800

    docs: add func spec for plot_price

---
+++ R/plot_price.R
@@ -0,0 +1,14 @@
+#' Plot the price of cryptocurrency
+#'
+#' @param price_df data frame with time and price data.
+#' @param name character name of the cryptocurrency of interest
+#' @param window vector of two dates in character 
+#'
+#' @return ggplot object
+#' @export
+#'
+#' @examples
+#' plot_price(price_df, "BTCBitcoin", ["2021-01-01", "2021-12-31"])
+plot_price <- function(price_df, name, window){
+  
+}
