7caa76f1fcdf0853b46be5b636a0d57c43182777
Berkay Bulut <mberkaybulut@gmail.com>
2022-01-20 21:01:18 -0500

    retrieve data description

---
+++ R/retrieve_data.R
@@ -0,0 +1,15 @@
+#' Retrieve the historical price of cryptocurrency
+#'
+#' @param symbol string of symbol for cryptocurrency
+#' @param time_period string of time period
+#' @param start_date string of start date in character
+#' @param end_date string of end date in character
+#'
+#' @return dataframe with time and price data.
+#' @export
+#'
+#' @examples
+#' retrieve_data("BTC-USDT", "1day", "2021-01-01", "2021-12-31")
+retrieve_data <- function(symbol, time_period, start_date, end_date){
+
+}
