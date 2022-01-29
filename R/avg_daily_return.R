eaf86877319093fc8daddec82c54333adf7b4768
YINAN GUO <alex.yinan.guo@gmail.com>
2022-01-29 04:04:49 -0800

    change docstring example of avg_daily_return

--- R/avg_daily_return.R
+++ R/avg_daily_return.R
@@ -5,7 +5,8 @@
 #' @return A numeric value representing the average daily return of the cryptocurrency.
 #'
 #' @examples
-#' df = data.frame('Date'=c('2022-01-15', '2022-01-16', '2022-01-17', '2022-01-18'), 'Price'=c(1.5, 2.5, 3.5, 4.5))
+#' df = data.frame('Date'=c('2022-01-15', '2022-01-16', '2022-01-17', '2022-01-18'),
+#'                 'Price'=c(1.5, 2.5, 3.5, 4.5))
 #' avg_daily_return(df$Price)
 avg_daily_return <- function(prices) {
   if(! is.double(prices)) {
