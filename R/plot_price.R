da6a832453f3da628f9173ef4c2e3d29ef26b4c1
Cici Du <67261289+ciciecho-ds@users.noreply.github.com>
2022-01-29 10:43:52 -0800

    Update R/plot_price.R
    Co-authored-by: nobbynguyen <89713246+nobbynguyen@users.noreply.github.com>

--- R/plot_price.R
+++ R/plot_price.R
@@ -6,7 +6,7 @@
 #' @export
 #'
 #' @examples
-#' price_df <- data.frame('Symbol'=c('BTC-USDT','BTC-USDT'), Date'=c('2022-01-15', '2022-01-16'), 'Close'=c(1.5, 2.5))
+#' price_df <- data.frame('Symbol'=c('BTC-USDT','BTC-USDT'), 'Date'=c('2022-01-15', '2022-01-16'), 'Close'=c(1.5, 2.5))
 #' plot_price(df = price_df)
 
 plot_price <- function(df = price_df){
