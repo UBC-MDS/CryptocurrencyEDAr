39e7d1141fa7956e597af8c90b1a3c786c1b3877
Cici Du <67261289+ciciecho-ds@users.noreply.github.com>
2022-01-28 19:01:19 -0800

    Update test-plot_price.R

--- tests/testthat/test-plot_price.R
+++ tests/testthat/test-plot_price.R
@@ -2,15 +2,15 @@ library(testthat)
 library(rlang)
 library(ggplot2)
 
-helper_df <- data.frame(Symbol = c("BTC-USDT", "BTC-USDT", "BTC-USDT"),
-Date = c(2021-12-25, 2021-12-26, 2021-12-27),
-Close = c(50771.5, 50715.7, 47543.8)
-)
-
-plot <- plot_price(helper_df)
-
 test_that('Plot should use geom_line and map Date to x-axis, Close to y-axis and
 title is not empty.', {
+    
+    helper_df <- data.frame(Symbol = c("BTC-USDT", "BTC-USDT", "BTC-USDT"),
+    Date = c(2021-12-25, 2021-12-26, 2021-12-27),
+    Close = c(50771.5, 50715.7, 47543.8)
+    )
+
+    plot <- plot_price(helper_df)
 
     expect_true("GeomLine" %in% c(class(plot$layers[[1]]$geom)))
 
